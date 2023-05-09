9e53c9bcfa94fe09e4e9c8e52c6487e8f28a471d
Donic <vadimdonik710@gmail.com>
2023-05-10 01:08:32 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/com/donic/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,63 @@
+package com.donic.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+//         Am declarat toate tipurile de variabile primitive.
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastChatFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperlimitOfDoubleDataType;
+
+//        Initializarea variabilelor declarate.
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastChatFromAscii = 'Z';
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperlimitOfDoubleDataType = 1.7E+305;
+
+        System.out.println(falseBooleanVariable);
+        System.out.println(trueBooleanVariable);
+        System.out.println(firstCharFromAscii);
+        System.out.println(lastChatFromAscii);
+        System.out.println(lowerLimitOfByteDataType);
+        System.out.println(upperLimitOfByteDataType);
+        System.out.println(lowerLimitOfShortDataType);
+        System.out.println(upperLimitOfShortDataType);
+        System.out.println(lowerLimitOfIntDataType);
+        System.out.println(upperLimitOfIntDataType);
+        System.out.println(lowerLimitOfLongDataType);
+        System.out.println(upperLimitOfLongDataType);
+        System.out.println(lowerLimitOfFloatDataType);
+        System.out.println(upperLimitOfFloatDataType);
+        System.out.println(lowerLimitOfDoubleDataType);
+        System.out.println(upperlimitOfDoubleDataType);
+
+    }
+
+
+}
