f637d10e355c8b0536d756c12b4e837e9643ab59
Donic Vadim <vadimdonik710@gmail.com>
2023-06-04 22:25:20 +0300

    Added the implementation of Checked and Unchecked exceptions.

---
+++ src/main/java/com/donic/exceptionsservicetask/ArithmeticOperationService.java
@@ -0,0 +1,17 @@
+package com.donic.exceptionsservicetask;
+
+public class ArithmeticOperationService {
+    public static int divideIntNoExceptionHandling (int a, int b){
+        return a / b;
+    }
+
+        public static int divideIntWithExceptionHandling (int a, int b){
+        try{
+            int result = a / b;
+            return result;
+        }catch (Exception capturedException){
+            System.out.println(" An exception has occured: " + capturedException.getMessage());
+            return 0;
+        }
+    }
+}
